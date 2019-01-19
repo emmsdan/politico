9667ed9679a26d836056224d12113b6b060a2019
Emmanuel Daniel <ecomje@gmail.com>
2019-01-20 00:13:21 +0100

    created gh-pages

---
+++ js/app.js
@@ -0,0 +1,74 @@
+window.focus();
+
+window.addEventListener('blur', () => {
+  window.location.reload();
+});
+
+window.addEventListener('load', (event) => {
+  slider();
+  modal();
+});
+
+// display random images (as sliders)
+const slider = () => {
+  const sliderImage = document.querySelector('.slider img');
+  if (!sliderImage) return;
+  const images = ['./img/nigerians-are-you-ready-to-Vote-in-2015-YouTube.png', './img/nigeria.png',  './img/voting.jpg', './img/nigerians-are-you-ready-to-Vote-in-2015-YouTube.png'];
+  let nextImg = 0;
+  const randomImage = (image) => {
+    sliderImage.src = image;
+    sliderImage.alt = image;
+  }
+  setInterval(()=> {
+    nextImg += 1;
+    randomImage(images[nextImg])
+    if (nextImg === (images.length - 1)) {
+      nextImg = 0
+    }
+  }, 5000)
+}
+
+const modal = () => {
+  const target = document.querySelector('[data-modal]').getAttribute('data-modal');
+  const modal = document.querySelector(target);
+  const trigger = document.querySelector("[data-modal]");
+  const closeButton = document.querySelector(".close");
+
+    function toggleModal() {
+        modal.classList.toggle("show");
+    }
+
+    const closeOnClick = (event) => {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", closeOnClick);
+}
+
+const loadHeader = ()  => {
+  const dataAdd = document.querySelectorAll('[data-add-file]');
+  console.log (dataAdd)
+  for (let files of dataAdd){
+    fetch (files.getAttribute('data-add-file'))
+    .then ( (response) => {
+      if (response.status === 200){
+
+        return response.text();
+      }
+      throw Error ('file does not exist');
+    })
+    .then ( (response) => {
+      files.removeAttribute('data-add-file');
+        return files.innerHTML = response;
+    })
+    .catch ((error) => {
+      files.setAttribute('data-error', error);
+      console.log (error);
+    })
+  }
+};
+
+loadHeader();
