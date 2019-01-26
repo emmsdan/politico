e640c8e96f1af1281fe9917528aaae4e9d02c510
Emmanuel Daniel <ecomje@gmail.com>
2019-01-27 00:35:49 +0100

    wrote unit test for politcal party route

--- server/controller/partyController.js
+++ server/controller/partyController.js
@@ -25,7 +25,7 @@ export default class partyController {
     const address = req.body.address || '<help>out';
     const logoUrl = req.body.logoUrl || 'null';
     try {
-      if (!validate.isAddress(address) || !validate.isName(name)) {
+      if (!validate.isAddress(address) || !address || !validate.isName(name) || !name) {
         return this.response({
           status: 406,
           message: 'please check input'
@@ -38,7 +38,7 @@ export default class partyController {
         }, null, res);
       }
       const newParty = {
-        partyId: new Date().getTime(),
+        partyId: req.body.partyID || new Date().getTime(),
         name,
         address,
         logoUrl,
@@ -67,13 +67,14 @@ export default class partyController {
   edit(req, res) {
     const id = req.params.partID;
     const name = null || req.body.name;
-    if (!validator.isInt(id)) {
+    if (!id || !validator.isInt(id)) {
       return this.response({
         status: 404,
         message: 'please provide a valid party id'
       }, null, res);
     }
-    if (!validate.isName(name)) {
+
+    if (!name || !validate.isName(name)) {
       return this.response({
         status: 404,
         message: 'invalid name, supplied'
@@ -81,6 +82,13 @@ export default class partyController {
     }
     const parties = this.database.filter(p => p.partyId !== Number(id));
     const thisParty = this.database.find(p => p.partyId === Number(id));
+    if (!thisParty) {
+      return this.response({
+        status: 404,
+        message: 'no registered party with such ID'
+      }, null, res);
+    }
+
     thisParty.name = name;
     thisParty.updatedOn = new Date().getTime();
     parties.push(thisParty);
@@ -111,6 +119,34 @@ export default class partyController {
   }
 
   /**
+   * get a specific political party
+   * @param {*} req
+   * @param {*} res
+   * @returns object;
+   */
+  get(req, res) {
+    const id = req.params.partID;
+    if (!id || !validator.isInt(id)) {
+      return this.response({
+        status: 404,
+        message: 'please provide a valid party id'
+      }, null, res);
+    }
+
+    const thisParty = this.database.find(p => p.partyId === Number(id));
+    if (!thisParty) {
+      return this.response({
+        status: 404,
+        message: 'no registered party with such ID'
+      }, null, res);
+    }
+    return this.response(null, {
+      status: 200,
+      message: thisParty
+    }, res);
+  }
+
+  /**
    * delete a political pearties
    * @param {*} req
    * @param {*} res
@@ -118,7 +154,7 @@ export default class partyController {
    */
   deleteParties(req, res) {
     const id = req.params.partID;
-    if (!validator.isInt(id)) {
+    if (!validator.isInt(id) || !id) {
       return this.response({
         status: 404,
         message: 'specify a valid party id.'
@@ -126,17 +162,16 @@ export default class partyController {
     }
 
     const party = this.database.filter(p => p.partyId !== Number(id));
-    console.log(party, id);
     if (party.length !== this.database.length) {
       this.database = party;
       return this.response(null, {
         status: 410,
-        message: { message: 'party delete successfully' }
+        message: 'party delete successfully'
       }, res);
     }
     return this.response({
       status: 404,
-      message: 'party id does not exist'
+      message: 'no registered party with such ID'
     }, null, res);
   }
 
--- test/party.datastructure.spec.js
+++ test/party.datastructure.spec.js
@@ -1,9 +1,10 @@
-import { chai, expect } from 'chai';
+import { expect } from 'chai';
 import request from 'supertest';
 import app from '../app';
 
 describe('Party Intergreted Test', () => {
   const party = {
+    partyID: 1234,
     name: 'Labour Party',
     logoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeEy3OA3OHeXypU1Hx94fSZIAYc5Lu1ZFjy_3o9g_qKaDKPZRH',
     address: 'Andela Epic tower, Ilupeju road, Lagos'
@@ -37,6 +38,78 @@ describe('Party Intergreted Test', () => {
         .end((err, res) => {
           expect(res.statusCode).to.equal(406);
           expect(res.body.error).equals('please check input');
+          done();
+        });
+    });
+  });
+
+  describe('#GET /api/v1/parties "view all political party."', () => {
+    it('should return a list all available political party', (done) => {
+      request(app).get('/api/v1/parties')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body.data).to.be.an('array');
+          done();
+        });
+    });
+
+    it('should return specific political party', (done) => {
+      request(app).get(`/api/v1/parties/${party.partyID}`)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body.data).to.be.an('array');
+          done();
+        });
+    });
+
+    it('should return invalid error', (done) => {
+      request(app).get('/api/v1/parties/wrong123')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(404);
+          expect(res.body.error).to.equal('please provide a valid party id');
+          done();
+        });
+    });
+  });
+
+  describe('#PATCH /api/v1/parties/partID/name "edit specific political party name."', () => {
+    it('should trigger an error', (done) => {
+      request(app).patch(`/api/v1/parties/${party.partyID}/name`)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(404);
+          expect(res.body.error).to.equal('invalid name, supplied');
+          done();
+        });
+    });
+
+    it('should return new party name', (done) => {
+      request(app).patch(`/api/v1/parties/${party.partyID}/name`)
+        .send({
+          name: 'Action congress'
+        })
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body.data).to.be.an('array');
+          done();
+        });
+    });
+  });
+
+  describe('#DELETE /api/v1/parties/partID "delete specific political party."', () => {
+    it('should return successful action', (done) => {
+      request(app).delete(`/api/v1/parties/${party.partyID}`)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(410);
+          expect(res.body.data[0]).to.equal('party delete successfully');
+          done();
+        });
+    });
+
+    it('should return party does not exist', (done) => {
+      request(app).get(`/api/v1/parties/${party.partyID}`)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(404);
+          expect(res.body.error).to.equal('no registered party with such ID');
           done();
         });
     });
