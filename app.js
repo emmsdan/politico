5b794ea1bec3cd79bd8753247914ebe3e5889d20
Emmanuel Daniel <ecomje@gmail.com>
2019-01-27 17:12:02 +0100

    changed port for heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import partyRouter from './server/route/partyRouter';
 import officeRouter from './server/route/officeRouter';
 
 const app = express();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 5051;
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
